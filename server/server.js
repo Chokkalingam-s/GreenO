99f38c916cd1c42d0bce9e6a59314022972c5694
Divakar G <divakar3501@gmail.com>
2024-09-05 15:56:16 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,39 @@
+
 const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to MySQL using Sequelize');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const app = express();
+app.use(express.json());
 
-app.use(cors());
 
-app.listen(8081, () => { 
-    console.log('Server running on port 8081');
+app.get('/', (req, res) => {
+  res.send('Welcome to the Green Community API!');
+});
+
+sequelize.sync({ force: false }) 
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch(err => {
+    console.error('Error synchronizing the database:', err);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
