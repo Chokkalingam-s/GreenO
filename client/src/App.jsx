8838575582728d24e53af1b76edc4f93753b3715
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-09-05 16:43:02 +0530

    SignIn / LogIn Ui - Wid Color Palatte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import SignIn from './components/signIn/SignIn'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <p>GreenO</p>
+    <SignIn />
     </>
   )
 }
---
+++ client/src/components/signIn/SignIn.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+import './SignIn.css'
+
+const SignIn = () => {
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
+
+
+  // Event handlers to switch between sign-in and sign-up modes
+  const handleSignUpClick = () => {
+    setIsSignUpMode(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpMode(false);
+  };
+
+  return (
+    <div className={`container ${isSignUpMode ? "sign-up-mode" : ""}`}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          {/* Sign In Form */}
+          <form action="#" className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+          </form>
+
+          {/* Sign Up Form */}
+          <form action="#" className="sign-up-form">
+            <h2 className="title">Sign up</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" className="btn" value="Sign up" />
+          </form>
+        </div>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here ?</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!
+            </p>
+            <button className="btn transparent" onClick={handleSignUpClick}>
+              Sign up
+            </button>
+          </div>
+          <img src="img/log.svg" className="image" alt="" />
+        </div>
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button className="btn transparent" onClick={handleSignInClick}>
+              Sign in
+            </button>
+          </div>
+          <img src="img/register.svg" className="image" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default SignIn
