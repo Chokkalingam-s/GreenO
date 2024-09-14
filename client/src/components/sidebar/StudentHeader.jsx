b1801924f78411f419ded4d550ea6cce0048aac6
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-09-14 16:22:16 +0530

    Header and Profile CSS

--- client/src/components/sidebar/StudentHeader.jsx
+++ client/src/components/sidebar/StudentHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
+import { BsFillBellFill, BsCameraFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
 
 
 function StudentHeader({ OpenSidebar }) {
@@ -9,11 +9,10 @@ function StudentHeader({ OpenSidebar }) {
         <BsJustify className="icon" onClick={OpenSidebar} />
       </div>
       <div className="header-left">
-        <BsSearch className="icon" />
       </div>
       <div className="header-right">
         <BsFillBellFill className="icon" />
-        <BsFillEnvelopeFill className="icon" />
+        <BsCameraFill className="icon" />
         <BsPersonCircle className="icon" />
       </div>
     </header>
