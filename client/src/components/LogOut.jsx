2b92fcc7da110d77c4cd1f103b9755590e760ea1
shanker2004 <shdeveloper2004@gmail.com>
2024-12-31 22:19:37 +0530

    fixed header

--- client/src/components/LogOut.jsx
+++ client/src/components/LogOut.jsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom'
-export default function Logout() {
+export default function Logout({ header = true }) {
   const navigate = useNavigate()
   function handleClick() {
     localStorage.removeItem('token')
     navigate('/signin')
   }
   return (
-    <button onClick={handleClick} className='hidden md:flex logout'>
+    <button
+      onClick={handleClick}
+      className={`${header ? 'hidden' : 'center'} md:flex logout`}>
       <img
         src='/arrow-right-to-bracket-solid.svg'
         alt='Logout Icon'
--- client/src/components/sidebar/StudentHeader.jsx
+++ client/src/components/sidebar/StudentHeader.jsx
@@ -2,9 +2,9 @@ import { LogOut } from '../../exp_components'
 
 export default function StudentHeader() {
   return (
-    <header className='w-full md:min-w-[60vw] rounded-xl flex items-center justify-between px-4 glassy md:fixed top-2 left-1/2 md:-translate-x-1/2 shadow-lg z-10 md:hidden'>
+    <header className='w-full md:min-w-[60vw] rounded-xl flex items-center justify-between px-4 glassy md:fixed top-2 left-1/2 md:-translate-x-1/2 shadow-lg z-10 md:hidden py-2'>
       <img src='/treegrow.png' alt='App Logo' className='w-10' />
-      <LogOut hide={true} />
+      <LogOut header={false} />
     </header>
   )
 }
