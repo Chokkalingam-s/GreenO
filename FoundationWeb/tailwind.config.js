5eec85706ee6094dffbc2967f55de56d2532b709
shanker2004 <shdeveloper2004@gmail.com>
2025-01-09 23:13:17 +0530

    initial folder structure & cleanup

---
+++ FoundationWeb/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: { primary: '#e9f9dc', secondary: '#aad8b0', tertiary: '#4c9a69' }
+    }
+  },
+  plugins: []
+}
