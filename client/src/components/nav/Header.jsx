d5b53ed8d8cf1b0546b9fed4433a6281055ef9ea
shanker2004 <shdeveloper2004@gmail.com>
2025-01-10 14:18:46 +0530

    logo fix

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -8,9 +8,9 @@ export default function Header() {
     role === 'hod' ? '/department' : role === 'admin' ? '/admin' : '/home'
 
   return (
-    <header className='w-full grid md:grid-cols-[10%_60%_10%] grid-cols-2 items-center md:justify-evenly md:justify-items-center fixed top-2 z-30 gap-x-36 md:gap-x-0 md:px-0'>
-      <NavLink className='glassy center round px-2' to={homeLink}>
-        <img src='/GreenO_Logo.png' alt='App Logo' className='w-14' />
+    <header className='w-full grid md:grid-cols-[10%_60%_10%] grid-cols-2 items-center md:justify-evenly md:justify-items-center fixed top-2 z-30 gap-x-36 md:gap-x-0 md:px-0 px-2'>
+      <NavLink className='glassy center round px-2 w-fit' to={homeLink}>
+        <img src='/GreenO_Logo.png' alt='App Logo' className='w-14 p-1' />
         <p className='font-medium hidden md:block'>GreenO</p>
       </NavLink>
       <NavBar role={role} />
