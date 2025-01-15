040d3eff04d0cf7a196b4e29cf239932e257db45
shanker2004 <shdeveloper2004@gmail.com>
2025-01-15 16:34:19 +0530

    cert miracle fix

--- client/src/student/Profile.jsx
+++ client/src/student/Profile.jsx
@@ -44,7 +44,7 @@ export default function Profile() {
             headers: { Authorization: `Bearer ${token}` }
           }
         )
-        console.log('Fetched Data:', response.data);
+        console.log('Fetched Data:', response.data)
         setStudentDetails(response.data)
         setLoading(false)
       } catch (err) {
@@ -56,6 +56,40 @@ export default function Profile() {
     fetchUploadedImagesCount()
     fetchStudentDetails()
   }, [])
+  const icons = {
+    person: {
+      viewBox: '0 0 448 512',
+      path: 'M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z'
+    },
+    email: {
+      viewBox: '0 0 512 512',
+      path: 'M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z'
+    },
+    phone: {
+      viewBox: '0 0 512 512',
+      path: 'M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z'
+    },
+    aad_card: {
+      viewBox: '0 0 576 512',
+      path: 'M64 32C28.7 32 0 60.7 0 96l0 32 576 0 0-32c0-35.3-28.7-64-64-64L64 32zM576 224L0 224 0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-192zM112 352l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16z'
+    },
+    school: {
+      viewBox: '0 0 576 512',
+      path: 'M288 0L400 0c8.8 0 16 7.2 16 16l0 64c0 8.8-7.2 16-16 16l-79.3 0 89.6 64L512 160c35.3 0 64 28.7 64 64l0 224c0 35.3-28.7 64-64 64l-176 0 0-112c0-26.5-21.5-48-48-48s-48 21.5-48 48l0 112L64 512c-35.3 0-64-28.7-64-64L0 224c0-35.3 28.7-64 64-64l101.7 0L256 95.5 256 32c0-17.7 14.3-32 32-32zm48 240a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM80 224c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0zm368 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zM80 352c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0zm384 0c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0z'
+    },
+    domain: {
+      viewBox: '0 0 448 512',
+      path: 'M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z'
+    },
+    badge: {
+      viewBox: '0 0 384 512',
+      path: 'M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-384c0-35.3-28.7-64-64-64L64 0zm96 320l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16L96 416c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM144 64l96 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-96 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z'
+    },
+    calendar: {
+      viewBox: '0 0 640 512',
+      path: 'M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9l0 28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5l0-24.6c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z'
+    }
+  }
 
   function handleClick() {
     if (progressPercentage < 100) {
@@ -71,6 +105,7 @@ export default function Profile() {
   }
 
   const handleGenerateCertificate = async () => {
+    toast.info('certificate download is in progress...')
     const certificateElement = document.querySelector('#certificate')
     if (!certificateElement) return
 
@@ -91,124 +126,137 @@ export default function Profile() {
       console.error('Error generating certificate:', error)
     } finally {
       certificateElement.classList.add('hidden')
+      toast.success('Download Complete...')
       setModal(!modal)
     }
+  }
+
+  const DetailItem = ({ label, value, icon }) => {
+    const { viewBox, path } = icons[icon]
+    console.log(viewBox, path)
+
+    return (
+      <li className='flex items-center gap-4'>
+        <svg viewBox={viewBox}>
+          <path d={path}></path>
+        </svg>
+        <div>
+          <p className='text-sm uppercase'>{label}</p>
+          <p className='font-medium'>{value}</p>
+        </div>
+      </li>
+    )
   }
 
   return (
     <>
       <div className='center mt-12 mb-20 md:mt-0 md:mb-0 flex-col'>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <h3 className='md:col-span-2 relative top-6 mb-6 pl-6 w-full text-2xl md:text-center'>
-            {studentDetails.name}&apos;s Profile
-          </h3>
-        )}
-        <div className='md:gap-4 grid grid-cols-1 md:grid-cols-2 mt-4 md:max-w-[60vw]'>
-          <div className='p-2 col-span-2 order-3 md:order-none'>
-            {loading ? (
-              <p>Loading...</p>
-            ) : error ? (
-              <p className='text-red-600'>Error: {error}</p>
-            ) : (
-              <div className='grid md:space-x-4 px-2 md:grid-cols-2 grid-cols-1'>
-                <section className='details_table'>
-                  <h4>Personal Details</h4>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Field</th>
-                        <th>Details</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Name</td>
-                        <td>{studentDetails.name}</td>
-                      </tr>
-                      <tr>
-                        <td>Email</td>
-                        <td>{studentDetails.email}</td>
-                      </tr>
-                      <tr>
-                        <td>Mobile Number</td>
-                        <td>{studentDetails.mobileNumber}</td>
-                      </tr>
-                      <tr>
-                        <td>Aadhar Number</td>
-                        <td>{studentDetails.aadharNumber}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </section>
-                <section className='details_table'>
-                  <h4>Educational Details</h4>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Field</th>
-                        <th>Details</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>College Name</td>
-                        <td>{studentDetails.collegeName}</td>
-                      </tr>
-                      <tr>
-                        <td>Department</td>
-                        <td>{studentDetails.department}</td>
-                      </tr>
-                      <tr>
-                        <td>College Register Number</td>
-                        <td>{studentDetails.collegeRegisterNumber}</td>
-                      </tr>
-                      <tr>
-                        <td>Year of Graduation</td>
-                        <td>{studentDetails.yearOfGraduation}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </section>
+        <h3 className='md:col-span-2 relative top-6 mb-6 pl-6 w-full text-2xl md:text-center'>
+          {studentDetails.name}&apos;s Profile
+        </h3>
+        <div className='md:max-w-[65vw] gap-x-2 center'>
+          <div className='outline-dotted'>
+            <div className='center flex-col gap-y-4 profile_tab'>
+              <div>
+                <h4 className='text-xl font-bold mb-4 border-b-2 border-secondary/50 pb-2'>
+                  Personal Details
+                </h4>
+                <ul className='space-y-4'>
+                  <DetailItem
+                    label='Name'
+                    value={studentDetails.name}
+                    icon='person'
+                  />
+                  <DetailItem
+                    label='Email'
+                    value={studentDetails.email}
+                    icon='email'
+                  />
+                  <DetailItem
+                    label='Mobile Number'
+                    value={studentDetails.mobileNumber}
+                    icon='phone'
+                  />
+                  <DetailItem
+                    label='Aadhar Number'
+                    value={studentDetails.aadharNumber}
+                    icon='aad_card'
+                  />
+                </ul>
               </div>
-            )}
+
+              <div>
+                <h4 className='text-xl font-bold mb-4 border-b border-white/50 pb-2'>
+                  Educational Details
+                </h4>
+                <ul className='space-y-4'>
+                  <DetailItem
+                    label='College Name'
+                    value={studentDetails.collegeName}
+                    icon='school'
+                  />
+                  <DetailItem
+                    label='Department'
+                    value={studentDetails.department}
+                    icon='domain'
+                  />
+                  <DetailItem
+                    label='College Register Number'
+                    value={studentDetails.collegeRegisterNumber}
+                    icon='badge'
+                  />
+                  <DetailItem
+                    label='Year of Graduation'
+                    value={studentDetails.yearOfGraduation}
+                    icon='calendar'
+                  />
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className='glassy round h-64 mb-6 w-11/12 mx-auto p-6 col-span-2 md:col-auto center'>
-            <Gauge
-              value={progressPercentage}
-              min={0}
-              max={100}
-              startAngle={-120}
-              endAngle={120}
-              thickness={4}
-              cornerRadius='50%'
-              sx={{
-                [`& .${gaugeClasses.valueText}`]: {
-                  fontSize: '30px',
-                  fontWeight: 'bold',
-                  color: '#fff'
-                },
-                [`& .MuiGauge-bar`]: { fill: '#4caf50' },
-                [`& .${gaugeClasses.valueArc}`]: { fill: '#fff' },
-                [`& .MuiGauge-background`]: { fill: '#e0e0e0' }
-              }}
-              text={() => `${progressPercentage1} / ${totalImages}`}
-            />
-            <p className='text-xl absolute bottom-10'>My Progress</p>
-          </div>
-          <div className='center flex-col w-11/12 h-64 mx-auto glassy round mb-16 md:mb-0'>
-            <img
-              src='/Certificate.png'
-              alt='certificate icon'
-              className='w-1/2 round'
-            />
-            <button
-              onClick={handleClick}
-              className={`${progressPercentage < 100 ? 'disabled' : ''}`}>
-              Generate Certificate
-            </button>
-          </div>
+          <span className='mx-auto grid grid-cols-2 md:col-span-2 gap-4 grid-rows-3'>
+            <div className='glassy round h-52 out'>
+              <Gauge
+                value={progressPercentage}
+                min={0}
+                max={100}
+                startAngle={-120}
+                endAngle={120}
+                thickness={4}
+                cornerRadius='50%'
+                sx={{
+                  [`& .${gaugeClasses.valueText}`]: {
+                    fontSize: '1.6rem',
+                    fontWeight: 'bold'
+                  },
+                  [`& .MuiGauge-bar`]: { fill: '#4caf50' },
+                  [`& .${gaugeClasses.valueArc}`]: { fill: '#aad8b0' },
+                  [`& .MuiGauge-background`]: { fill: '#e0e0e0' }
+                }}
+                text={() => `${progressPercentage1} / ${totalImages}`}
+              />
+              <p className='text-lg absolute bottom-1.5 w-full text-center'>
+                My Progress
+              </p>
+            </div>
+            <div className='center flex-col h-52 glassy round out'>
+              <img
+                src='/Certificate.png'
+                alt='certificate icon'
+                className='w-8/12 round'
+              />
+              <button
+                onClick={handleClick}
+                className={`${
+                  progressPercentage < 100 ? 'disabled' : ''
+                } w-11/12`}>
+                Certificate
+              </button>
+            </div>
+            <div className='col-span-2 row-span-3 mt-2 p-2 glassy round out'>
+              <h2>Settings</h2>
+            </div>
+          </span>
         </div>
       </div>
 
@@ -216,21 +264,23 @@ export default function Profile() {
         <div className='glassy h-full inset-0 md:w-10/12 md:top-1/2 md:-translate-y-1/2 mx-auto py-2 z-20 fixed'>
           <div
             id='certificate'
-            className='md:w-[297mm] md:h-[210mm] relative top-1/2 md:top-0 -translate-y-1/2 md:-translate-y-0'>
+            className='md:w-[297mm] md:h-[210mm] relative top-1/2 md:top-0 -translate-y-1/2 md:-translate-y-0 center'>
             <img
               src='/Certificate.png'
               alt='Certificate Background'
               className='w-full h-full'
             />
-            <div className='absolute z-20 top-1/2 left-1/2 transform -translate-x-[30vw] -translate-y-1/2 mt-2 text-center text-black font-medium  w-full'>
+            <div className='absolute text-black text-center'>
               {studentDetails ? (
                 <span className='cert'>
-                  <h2 className='transform -translate-x-[7vw]'>{studentDetails.name}</h2>
+                  <h2>{studentDetails.name}</h2>
                   <p>
-                    Student from Department of <strong> {studentDetails.department}</strong>, <strong>{studentDetails.collegeName}</strong>
+                    From Department of
+                    <strong> {studentDetails.department}</strong>,
+                    <strong> {studentDetails.collegeName}</strong>
                   </p>
                   <p>
-                    Successfully Grown a Tree in academic period 
+                    Successfully Grown a Tree in academic period
                     <br />
                     <strong>
                       {studentDetails.yearOfGraduation - 4} -
@@ -243,9 +293,9 @@ export default function Profile() {
               )}
             </div>
           </div>
-          <div className='md:w-fit absolute md:right-0 bottom-0 md:bg-gradient-to-r from-transparent to-black/15 md:h-full center flex-col p-2 right-1/2 translate-x-1/2 md:translate-x-0 top-1/2 md:top-0'>
+          <div className='absolute grid grid-cols-2 gap-x-10 w-10/12 left-1/2 -translate-x-1/2 bottom-1/4 md:bottom-20 md:w-fit md:grid-cols-1 glassy md:px-2 round out'>
             <button onClick={handleGenerateCertificate}>Download PDF</button>
-            <button onClick={() => setModal(!modal)} className='cancel w-full'>
+            <button onClick={() => setModal(!modal)} className='cancel'>
               Cancel
             </button>
           </div>
--- client/src/student/data.js
+++ client/src/student/data.js
@@ -18,7 +18,7 @@ export const plantGrowthData = [
         "In Stage 3, the plant transitions into its reproductive phase, which is marked by the production of flowers and fruits. This stage signifies maturity as the plant begins to focus on reproduction and the continuation of its species. Flowers play a key role in attracting pollinators such as bees, butterflies, and birds, which facilitate the transfer of pollen. This process is critical for fertilization and the formation of seeds. During this stage, the plant diverts its energy from vegetative growth to the development of flowers, fruits, and seeds. Fruits act as protective structures for seeds and aid in their dispersal, ensuring that the plant's genetic material is spread to new locations for future growth. The plant’s ability to produce flowers and fruits depends on adequate sunlight, water, and nutrients, as well as favorable environmental conditions. This phase represents the culmination of the plant's growth cycle, balancing the energy between sustaining itself and generating new life."
     }
   ];
-  
+
   export const videoData = [
     {
       id: 1,
@@ -33,4 +33,38 @@ export const plantGrowthData = [
       description: "Major Mistakes We Usually Do"
     }
   ];
-  
+
+const icons = {
+  person: {
+    viewBox: '0 0 448 512',
+    path: 'M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z'
+  },
+  email: {
+    viewBox: '0 0 512 512',
+    path: 'M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z'
+  },
+  phone: {
+    viewBox: '0 0 512 512',
+    path: 'M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z'
+  },
+  creditcard: {
+    viewBox: '0 0 576 512',
+    path: 'M64 32C28.7 32 0 60.7 0 96l0 32 576 0 0-32c0-35.3-28.7-64-64-64L64 32zM576 224L0 224 0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-192zM112 352l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16z'
+  },
+  school: {
+    viewBox: '0 0 576 512',
+    path: 'M288 0L400 0c8.8 0 16 7.2 16 16l0 64c0 8.8-7.2 16-16 16l-79.3 0 89.6 64L512 160c35.3 0 64 28.7 64 64l0 224c0 35.3-28.7 64-64 64l-176 0 0-112c0-26.5-21.5-48-48-48s-48 21.5-48 48l0 112L64 512c-35.3 0-64-28.7-64-64L0 224c0-35.3 28.7-64 64-64l101.7 0L256 95.5 256 32c0-17.7 14.3-32 32-32zm48 240a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM80 224c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0zm368 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zM80 352c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0zm384 0c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-32 0z'
+  },
+  domain: {
+    viewBox: '0 0 448 512',
+    path: 'M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z'
+  },
+  badge: {
+    viewBox: '0 0 384 512',
+    path: 'M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-384c0-35.3-28.7-64-64-64L64 0zm96 320l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16L96 416c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM144 64l96 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-96 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z'
+  },
+  calendar: {
+    viewBox: '0 0 640 512',
+    path: 'M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9l0 28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5l0-24.6c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1c8.1 0 16.1-1.4 23.7-4.1L624.2 198.6c9.5-3.5 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L320 36.1C336.1 33.3 328.1 32 320 32z'
+  }
+}
