7a8daa37f617025c02007fd2f0edcb50cca8c4d4
shanker2004 <shdeveloper2004@gmail.com>
2025-01-24 12:28:38 +0530

    foundation landing page

---
+++ FoundationWeb/src/components/Branch.jsx
@@ -0,0 +1,8 @@
+export default function Branch() {
+  return (
+    <>
+      <div className='w-4 h-4 bg-tertiary rounded-full'></div>
+      <div className='h-16 w-1 bg-secondary'></div>
+    </>
+  )
+}
