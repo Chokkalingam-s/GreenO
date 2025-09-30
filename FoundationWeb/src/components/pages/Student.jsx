864f5d350746eb4b7dbb6cb0bd90c9a5d26dfd9d
Chokkalingam.s <chokka7878@gmail.com>
2025-09-30 19:32:46 +0530

    student page get involved content push

--- FoundationWeb/src/components/pages/Student.jsx
+++ FoundationWeb/src/components/pages/Student.jsx
@@ -3,8 +3,27 @@ import DropDownLayout from './DropDownLayout'
 export default function Student() {
   return (
     <DropDownLayout
-      title='For Students'
-      content='Stay ahead in your studies with tools for scheduling, note-taking, and collaboration. No more last-minute chaos.'
+      title='For Students - Your small hands can make a giant change.'
+      content='Students are the future changemakers! At CG Foundation, your energy, creativity, and dedication can make a tangible difference. Whether it’s planting a tree through GreenO, conducting awareness drives, or participating in skill-building workshops—you can spark ripples of transformation in your community.
+
+How You Can Contribute:
+
+GreenO Tree Planting: Plant, nurture, and track saplings with geo-verified and AI-supported validation.
+
+Volunteering Programs: Join campaigns in education, health awareness, and community services.
+
+Innovative Ideas: Share projects that solve local problems.
+
+Impact & Benefits:
+
+Enhance your leadership and organizational skills.
+
+Earn recognition, certificates, and awards for your contribution.
+
+Be part of a nationwide movement of conscious students.
+
+Call to Action:
+Ready to make an impact? Fill out the contact form below to get started!'
       img='https://images.unsplash.com/photo-1755354567507-10dae25beb9a?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     />
   )
